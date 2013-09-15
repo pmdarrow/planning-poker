cec1f49ba4fe98183139987f94826a45b2d5caed
Peter Darrow <peter@sheepdoginc.ca>
2013-09-15 16:12:30 -0300

    Enable editing of stories.

--- js/views/story-view.js
+++ js/views/story-view.js
@@ -1,84 +1,49 @@
 var app = app || {};
 
 (function ($) {
-	app.StoryView = Backbone.View.extend({
-		tagName: 'a',
+  app.StoryView = Backbone.View.extend({
+    tagName: 'a',
     className: 'list-group-item',
-		template: _.template($('#story-template').html()),
+    template: _.template($('#story-template').html()),
     attributes: {'href': '#'},
-		events: {
-			'click .toggle': 'toggleCompleted',
-			'dblclick label': 'edit',
-			'click .destroy': 'clear',
-			'keypress .edit': 'updateOnEnter',
-			'blur .edit': 'close'
-		},
+    events: {
+      'dblclick': 'edit',
+      'submit .edit': 'update'
+    },
 
-		// The StoryView listens for changes to its model, re-rendering. Since there's
-		// a one-to-one correspondence between a Story and a StoryView in this
-		// app, we set a direct reference on the model for convenience.
-		initialize: function () {
-			this.listenTo(this.model, 'change', this.render);
-			this.listenTo(this.model, 'destroy', this.remove);
-			this.listenTo(this.model, 'visible', this.toggleVisible);
-		},
+    initialize: function () {
+      this.listenTo(this.model, 'change', this.render);
+      this.listenTo(this.model, 'destroy', this.remove);
+      this.listenTo(this.model, 'visible', this.toggleVisible);
+    },
 
-		// Re-render the titles of the story item.
-		render: function () {
-			this.$el.html(this.template(this.model.toJSON()));
-			this.$el.toggleClass('completed', this.model.get('completed'));
-			this.toggleVisible();
-			this.$input = this.$('.edit');
-			return this;
-		},
+    render: function () {
+      this.$el.html(this.template(this.model.toJSON()));
+      this.$title = this.$('#story-title');
+      this.$description = this.$('#story-description');
+      return this;
+    },
 
-		toggleVisible: function () {
-			this.$el.toggleClass('hidden', this.isHidden());
-		},
+    edit: function (e) {
+      this.$el.addClass('editing');
+      this.$title.focus();
+      return false;
+    },
 
-		isHidden: function () {
-			var isCompleted = this.model.get('completed');
-			return (// hidden cases only
-				(!isCompleted && app.StoryFilter === 'completed') ||
-				(isCompleted && app.StoryFilter === 'active')
-			);
-		},
+    update: function () {
+      var trimmedTitle = this.$title.val().trim();
+      var trimmedDescription = this.$description.val().trim();
+      this.$title.val(trimmedTitle);
+      this.$description.val(trimmedDescription);
 
-		// Toggle the `"completed"` state of the model.
-		toggleCompleted: function () {
-			this.model.toggle();
-		},
+      if (trimmedTitle) {
+        this.model.save({
+          title: trimmedTitle,
+          description: trimmedDescription
+        });
+      }
 
-		// Switch this view into `"editing"` mode, displaying the input field.
-		edit: function () {
-			this.$el.addClass('editing');
-			this.$input.focus();
-		},
-
-		// Close the `"editing"` mode, saving changes to the story.
-		close: function () {
-			var trimmedValue = this.$input.val().trim();
-			this.$input.val(trimmedValue);
-
-			if (trimmedValue) {
-				this.model.save({ title: trimmedValue });
-			} else {
-				this.clear();
-			}
-
-			this.$el.removeClass('editing');
-		},
-
-		// If you hit `enter`, we're through editing the item.
-		updateOnEnter: function (e) {
-			if (e.which === ENTER_KEY) {
-				this.close();
-			}
-		},
-
-		// Remove the item, destroy the model from *localStorage* and delete its view.
-		clear: function () {
-			this.model.destroy();
-		}
-	});
+      this.$el.removeClass('editing');
+    }
+  });
 })(jQuery);
