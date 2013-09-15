fefdc525cd140bb1d65f72599d30eeb37939c12a
Peter Darrow <peter@sheepdoginc.ca>
2013-09-15 17:19:05 -0300

    More cleanup and hook up GoInstant.

--- js/views/app-view.js
+++ js/views/app-view.js
@@ -4,6 +4,8 @@ var app = app || {};
   app.AppView = Backbone.View.extend({
     el: '#planning-poker-app',
     statsTemplate: _.template($('#stats-template').html()),
+    goInstantUrl: 'https://goinstant.net/pmdarrow/planning-poker',
+    goInstantRoom: 'planning-poker',
     events: {
       'submit #create-story-form': 'createOnSubmit'
     },
@@ -11,20 +13,34 @@ var app = app || {};
     initialize: function () {
       this.$title = this.$('#new-story-title');
       this.$description = this.$('#new-story-description');
-      this.$createStory = this.$('#create-story');
       this.$footer = this.$('#footer');
       this.$main = this.$('#main');
 
       this.listenTo(app.stories, 'add', this.addOne);
 			this.listenTo(app.stories, 'reset', this.addAll);
-//			this.listenTo(app.stories, 'change:revealed', this.filterOne);
-//			this.listenTo(app.stories, 'filter', this.filterAll);
-//			this.listenTo(app.stories, 'all', this.render);
 
-      // Suppresses 'add' events with {reset: true} and prevents the app view
-      // from being re-rendered for every model. Only renders when the 'reset'
-      // event is triggered at the end of the fetch.
+      this.initGoInstant();
+
       app.stories.fetch({reset: true});
+    },
+
+    initGoInstant: function() {
+      this.platform = new goinstant.Platform(this.goInstantUrl);
+      this.platform.connect(_.bind(function(err) {
+        if (err) {
+          console.log("Error connecting to GoInstant:", err);
+          return;
+        }
+        console.log("Connected to GoInstant");
+        this.room = this.platform.room(this.goInstantRoom);
+        this.room.join(function(err, room) {
+          if (err) {
+            console.log('Error joining room:', err);
+            return;
+          }
+          console.log('Joined room:', room);
+        });
+      }, this));
     },
 
     // Re-rendering the App just means refreshing the statistics -- the rest
@@ -52,25 +68,14 @@ var app = app || {};
       }
     },
 
-    // Add a single story to the list by creating a view for it, and
-    // appending its element to the `<ul>`.
     addOne: function (story) {
       var view = new app.StoryView({ model: story });
       $('#story-list').append(view.render().el);
     },
 
-    // Add all items in the Stories collection at once.
     addAll: function () {
       this.$('#story-list').html('');
       app.stories.each(this.addOne, this);
-    },
-
-    filterOne: function (story) {
-      story.trigger('visible');
-    },
-
-    filterAll: function () {
-      app.stories.each(this.filterOne, this);
     },
 
     newAttributes: function () {
@@ -91,21 +96,5 @@ var app = app || {};
       this.$description.val('');
       return false;
     }
-
-    // Clear all completed todo items, destroying their models.
-//		clearCompleted: function () {
-//			_.invoke(app.stories.completed(), 'destroy');
-//			return false;
-//		},
-//
-//		toggleAllComplete: function () {
-//			var completed = this.allCheckbox.checked;
-//
-//			app.stories.each(function (story) {
-//				story.save({
-//					'completed': completed
-//				});
-//			});
-//		}
   });
 })(jQuery);
