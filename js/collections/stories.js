25bd61b22d5a9bea804f3114446ae0f644e927a8
Peter Darrow <peter@sheepdoginc.ca>
2013-09-15 16:12:09 -0300

    Fix weird indentation from todo scaffold.

--- js/collections/stories.js
+++ js/collections/stories.js
@@ -1,31 +1,31 @@
 var app = app || {};
 
 (function () {
-	var Stories = Backbone.Collection.extend({
-		model: app.Story,
-		localStorage: new Backbone.LocalStorage('planning-poker-backbone'),
+  var Stories = Backbone.Collection.extend({
+    model: app.Story,
+    localStorage: new Backbone.LocalStorage('planning-poker-backbone'),
 
-		revealed: function () {
-			return this.filter(function (story) {
-				return story.get('estimateRevealed');
-			});
-		},
+    revealed: function () {
+      return this.filter(function (story) {
+        return story.get('estimateRevealed');
+      });
+    },
 
-		remaining: function () {
-			return this.without.apply(this, this.revealed());
-		},
+    remaining: function () {
+      return this.without.apply(this, this.revealed());
+    },
 
-		nextOrder: function () {
-			if (!this.length) {
-				return 1;
-			}
-			return this.last().get('order') + 1;
-		},
+    nextOrder: function () {
+      if (!this.length) {
+        return 1;
+      }
+      return this.last().get('order') + 1;
+    },
 
-		comparator: function (story) {
-			return story.get('order');
-		}
-	});
+    comparator: function (story) {
+      return story.get('order');
+    }
+  });
 
-	app.stories = new Stories();
+  app.stories = new Stories();
 })();
